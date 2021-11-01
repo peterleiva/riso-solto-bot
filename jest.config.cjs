2ca7e981376e59e74fb3e230579103734a91d40c
Peter <github@peter.dev.br>
2021-11-01 18:33:04 -0300

    Initiate project structure

---
+++ jest.config.cjs
@@ -0,0 +1,19 @@
+const { resolve } = require("path");
+
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+module.exports = {
+  collectCoverage: true,
+  coverageProvider: "v8",
+  coverageDirectory: resolve(__dirname, "coverage"),
+
+  rootDir: "./src",
+  testEnvironment: "node",
+
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname",
+  ],
+};
